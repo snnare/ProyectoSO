bac7390a678dd2e499684c2e006637aac7689193
Jose Rios <68526429+snnare@users.noreply.github.com>
2023-11-20 13:22:52 -0600

    Update cliente.py
    Modificacion del cliente

--- cliente.py
+++ cliente.py
@@ -1,43 +1,88 @@
 import socket
-import random
+import cv2
+import numpy as np
 import time
 
-# Configura el cliente
-host = '172.26.48.117'  # Utiliza la dirección IP local de la misma computadora
-port = 12345
+def connect_to_server():
+    host = '127.0.0.1'
+    port = 12345
 
-# Crea un socket del cliente
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((host, port))
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((host, port))
 
-# Función para agregar un carácter aleatorio al texto
-def agregar_caracter_aleatorio(texto):
-    caracteres = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-    caracter_aleatorio = random.choice(caracteres)
-    return texto + caracter_aleatorio
+    print(f"Conectado al servidor en {host}:{port}")
+    return client_socket
 
-try:
-    texto_recibido = client_socket.recv(1024).decode()
-    print(f"Texto recibido del servidor: {texto_recibido}")
+def send_image(client_socket, image):
+    _, img_encoded = cv2.imencode('.png', image)
+    img_bytes = img_encoded.tobytes()
 
-    while True:
-        texto_modificado = agregar_caracter_aleatorio(texto_recibido)
-        print(f"Texto modificado: {texto_modificado}")
+    size = len(img_bytes)
+    send_message(client_socket, str(size))
 
-        # Envía la respuesta al servidor
-        client_socket.send(texto_modificado.encode())
+    client_socket.sendall(img_bytes)
+    print("Imagen enviada al servidor")
 
-        # Agrega una pausa de 2 segundos
-        time.sleep(2)
+def receive_image(client_socket):
+    size_str = receive_message(client_socket)
+    size = int(size_str)
 
-        texto_recibido = client_socket.recv(1024).decode()
-        print(f"Texto recibido del servidor: {texto_recibido}")
+    img_bytes = client_socket.recv(size)
+    print("Imagen recibida del servidor")
 
-        # Agrega una pausa de 2 segundos
-        time.sleep(2)
+    img_array = np.frombuffer(img_bytes, dtype=np.uint8)
+    image = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
 
-except KeyboardInterrupt:
-    print("Servidor desconectado.")
+    return image
 
-# Cierra la conexión
-client_socket.close()
+def send_message(client_socket, message):
+    client_socket.sendall(message.encode('utf-8'))
+
+def receive_message(client_socket):
+    data = client_socket.recv(1024)
+    return data.decode('utf-8')
+
+def cargar_imagen(path):
+    return cv2.imread(path)
+
+def guardar_imagen(filename, image):
+    cv2.imwrite(filename, image)
+
+def mostrar_imagen(titulo, image, tiempo_espera=1000, tiempo_sleep=0.5):
+    cv2.imshow(titulo, image)
+    cv2.waitKey(tiempo_espera)
+    time.sleep(tiempo_sleep)
+
+def modificar_imagen(image):
+    matriz_rotacion_original = cv2.getRotationMatrix2D((image.shape[1] // 2, image.shape[0] // 2), 45, 1)
+    return cv2.warpAffine(image, matriz_rotacion_original, (image.shape[1], image.shape[0]))
+
+def close_connection(client_socket):
+    client_socket.close()
+
+def main():
+    client_socket = connect_to_server()
+
+    num_iterations = int(receive_message(client_socket))
+    print(f"Recibido el número de iteraciones: {num_iterations}")
+
+    for iteration in range(1, num_iterations + 1):
+        received_image = receive_image(client_socket)
+       
+        mostrar_imagen(f"Imagen recibida en iteración {iteration}", received_image)
+
+        modified_image = modificar_imagen(received_image)
+
+        guardar_imagen(f"Enviadas/IMG_server{iteration}.png", modified_image)
+
+        if received_image is not None and received_image.shape[0] > 0 and received_image.shape[1] > 0:
+            mostrar_imagen(f"Imagen recibida en iteración {iteration}", received_image)
+        else:
+            print(f"Imagen recibida en iteración {iteration} no válida")
+
+        send_image(client_socket, modified_image)
+
+    client_socket.close()
+
+if __name__ == "__main__":
+    main()
